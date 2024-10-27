6d9f1089f8750a902f25d65e56c056d6f55d337c
heiditeng <heidi.c.teng@gmail.com>
2024-10-27 13:43:44 -0400

    fixed frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
